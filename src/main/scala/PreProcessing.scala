f69c12db3f555a00196de46ef2b6c2973359edee
Andrew Smith <andrewts129@gmail.com>
2020-12-10 22:08:35 -0500

    Add OpenNLP detokenization

--- src/main/scala/PreProcessing.scala
+++ src/main/scala/PreProcessing.scala
@@ -1,7 +1,8 @@
 import cats.effect.IO
 import fs2.{Pure, Stream}
 import opennlp.tools.postag.{POSModel, POSTaggerME}
-import opennlp.tools.tokenize.SimpleTokenizer
+import opennlp.tools.tokenize.DetokenizationDictionary.Operation
+import opennlp.tools.tokenize.{DetokenizationDictionary, DictionaryDetokenizer, SimpleTokenizer}
 
 object PreProcessing {
   case class PosToken(token: String, pos: String) {
@@ -11,6 +12,26 @@ object PreProcessing {
   private lazy val tokenizer = SimpleTokenizer.INSTANCE
   private lazy val taggerModel = new POSModel(getClass.getResourceAsStream("en-pos-maxent.bin"))  // TODO download this
   private lazy val tagger = new POSTaggerME(taggerModel)
+
+  private val NO_SPACE_BEFORE = Array(".", ",", "!", "?", ";", ":", "%", ")", "]", "}", "”")
+  private val NO_SPACE_AFTER = Array("(", "[", "{", "$", "“", "#")
+  private val NO_SPACE_EITHER = Array("-", "_", "’")
+  private val ALTERNATING = Array("\"", "'")
+
+  private lazy val detokenizer = {
+    val instructions: Array[(String, Operation)] =
+      NO_SPACE_BEFORE.map((_, Operation.MOVE_LEFT)) ++
+      NO_SPACE_AFTER.map((_, Operation.MOVE_RIGHT)) ++
+      NO_SPACE_EITHER.map((_, Operation.MOVE_BOTH)) ++
+      ALTERNATING.map((_, Operation.RIGHT_LEFT_MATCHING))
+
+    val dict = new DetokenizationDictionary(
+      instructions.map(_._1),
+      instructions.map(_._2)
+    )
+
+    new DictionaryDetokenizer(dict)
+  }
 
   def all(string: String, ngramSize: Int): Stream[Pure, Vector[PosToken]] = {
     asNGrams(posTag(tokenize(string)), ngramSize)
@@ -30,7 +51,7 @@ object PreProcessing {
   }
 
   def detokenize(tokens: Vector[String]): String = {
-    tokens.mkString(" ")
+    detokenizer.detokenize(tokens.toArray, null)
   }
 
   def detokenize(posTokens: Stream[IO, PosToken]): Stream[IO, String] = {
